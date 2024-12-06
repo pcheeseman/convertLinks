d8f99fa215760f25bb1152b3c398835024d0afc0
pcheeseman <pcheeseman@brookes.ac.uk>
2024-12-06 10:50:08 +0000

    Add files via upload
    Credit to @AndreBradshaw - Sourcing Support, for his video that explains 'How to Extract Hidden Links from Cells in Google Sheets': 
    https://youtu.be/d1SZyoC9wWY?si=MAaEHpZ9v6oNFIj- 
    and the one that preceded it: 
    https://youtu.be/ZmdN51__4_I?si=V9qYifmJppM4qwyD

---
+++ convertLinks.js
@@ -0,0 +1,25 @@
+var your_sheet_id = '1RuhHfpVq86kvUMmLXHYDSG8ZQSq77TpjTMag_ZwqQPM';
+var your_sheet_name = 'Sheet1';
+
+var ss = SpreadsheetApp.openById(your_sheet_Id);
+var report = ss.getSheetByName(your_sheet_name);
+
+function convertLinks() {
+    var header = Array.from(report.getRange(1, 1, 1, report.getLastColumn()).getValues())[0];
+    var rows = report.getLastRow();
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map 
+    var cols_with_links = header.map((i,n,r) => { //i == the current value on the iteration. n an the index, r == the full array/header
+        var links = Array.rom(report.getRange(1, (n+1),rows, 1) .getRichTextValues()).map(row=> [row[0].getLinkUrl()]);
+        links.shift();
+        var head = [[('url_'+1)]];
+        var new_col = [...head,...links];
+        return {
+            has_links: links.some(v=> v[0]), // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+            new_col: new_col
+        }
+    }).filter(r=> r.has_links);
+    
+    cols_with_links.forEach(col=> {
+        report.getRange(1, report.getLastColumn()+1, col.new_col.length, 1).setValues(col.new_col);
+    })
